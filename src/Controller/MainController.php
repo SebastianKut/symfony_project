f1fe142b605b82ec3e54bfaedddfdd0a452f5719
Sebastian Kut <sebastian_kut@yahoo.co.uk>
2020-11-20 20:51:55 +0100

    custom function

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -19,14 +20,19 @@ class MainController extends AbstractController
         return new Response('<h1>Yo</h1>');
     }
 
+
+    //set up route that will have parameter that can be changed dynamically
     /**
-     * @Route("/custom", name="custom")
+     * @Route("/custom/{name?}", name="custom")             //add parameter to route {name} - to make it optional add ? so it looks like this {name?}
+     * @param Request
+     * @return Response
      */
     
-     public function custom() 
+     public function custom( Request $response ) 
      {
-
-        return new Response( 'This is custom page' );
+        //dump( $response->get('name') ); //similar to var_dump - dump() is symfony function that formats stuff nicely
+        $name = $response->get('name');
+        return new Response( '<h1>Welcome '.$name. '!</h1>' );
      }
 
 
